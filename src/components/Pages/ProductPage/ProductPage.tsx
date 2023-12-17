c96ca37466581eb06214d34cb8de6a2a09cbd161
Radif <120592479+RadifK@users.noreply.github.com>
2023-12-17 14:11:13 +0300

    minor fixes

--- src/components/Pages/ProductPage/ProductPage.tsx
+++ src/components/Pages/ProductPage/ProductPage.tsx
@@ -23,7 +23,7 @@ const ProductPage: FC = () => {
 	}
 	const [{ id, name, price, isLiked, images }] = product
 
-	const [descView, setDescView] = useState(false)
+	// сюда стейт
 
 
 
@@ -32,7 +32,7 @@ const ProductPage: FC = () => {
 			<div className={s.product}>
 				<div className={s.like}><ToFavorites id={id} isLiked={isLiked} /></div>
 				<div className={s.images}>
-				<Slider>
+					<Slider>
 						{images}
 					</Slider>
 				</div>
@@ -42,43 +42,7 @@ const ProductPage: FC = () => {
 				<p className={s.price}>{`${price} ₽`}</p>
 			</div>
 
-			<div className={s.description}>
-				<div className={s.descTitle}>
-					<p>
-						Описание и характеристики
-					</p>
-					<div className={!descView ? s.chevrone
-						:
-						`${s.chevrone} ${s.chevroneOpened}`}>
-						<FaChevronDown
-							onClick={() => { setDescView(!descView) }} />
-					</div>
-				</div>
-
-
-				{descView ?
-					<ul >
-						<li>Активное шумоподавление: Нет</li>
-						<li>Вес: 10 гр</li>
-						<li>Влагозащита: Нет</li>
-						<li>Длина кабеля: 1.2 м</li>
-						<li>Комплектация: Наушники</li>
-						<li>Материал корпуса: Пластик, резина</li>
-						<li>Микрофон: Да</li>
-						<li>Назначение: Проводные наушники</li>
-						<li>Ответить/закончить разговор: Да</li>
-						<li>Разъем наушников: Lightning</li>
-						<li>Тип крепления: Без крепления</li>
-						<li>Тип наушников: Вкладыши ("таблетки")</li>
-						<li>Тип подключения: Проводное</li>
-						<li>Частотный диапазон: 20 Гц - 20000 Гц</li>
-						<li>Чувствительность: 109 дБ</li>
-					</ul>
-					:
-					<>
-					</>}
-
-			</div>
+			{/* сюда description */}
 
 			<Reviews id={id} />
 			<div className={s.buttons}>
